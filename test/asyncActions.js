313cd7398cca95d8c2ed80cbd8bb20d83db9a1f7
Duncan Regan <duncanregan@gmail.com>
2016-02-20 21:38:21 -0500

    creates async test for update current video

--- test/asyncActions.js
+++ test/asyncActions.js
@@ -1,1 +1,25 @@
+import Immutable from 'immutable';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { UPDATE_CURRENT_VIDEO, updateCurrentVideo } from '../js/actions/currentVideo';
+import range from '../js/utils/range';
 
+const mockStore = configureMockStore([thunk])
+const mockVideos = Immutable.List(range(1, 15).map(i => {
+  return Immutable.Map({title: `Video ${i}`});
+}));
+
+describe('Async Action Creators', () => {
+  describe('currentVideo', () => {
+    describe('updateCurrentVideo', (done) => {
+      it('should create an action to update the current video', () => {
+        const index = 9;
+        const video = mockVideos.get(index);
+        const expectedActions = [{type: UPDATE_CURRENT_VIDEO, video}];
+        const store = mockStore({videos: mockVideos}, expectedActions, done);
+
+        store.dispatch(updateCurrentVideo(index));
+      })
+    })
+  })
+})
