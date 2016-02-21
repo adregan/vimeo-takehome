d4787ecde008fc7d795ab991681d5fa533c5e4b5
Duncan Regan <duncanregan@gmail.com>
2016-02-20 21:52:32 -0500

    adds a video reducer test

--- test/reducers.js
+++ test/reducers.js
@@ -1,7 +1,10 @@
 import expect from 'expect'
 import Immutable from 'immutable';
+import range from '../js/utils/range';
 import { UPDATE_CHANNEL } from '../js/actions/channel';
 import { channel } from '../js/reducers/channel';
+import { UPDATE_VIDEOS } from '../js/actions/videos';
+import { videos } from '../js/reducers/videos';
 
 describe('Reducers', () => {
   describe('channel', () => {
@@ -19,6 +22,21 @@ describe('Reducers', () => {
       });
       expect(channel(state, {type: UPDATE_CHANNEL, channel: exampleChannel}))
         .toEqual(exampleChannel);
+    })
+  })
+  describe('videos', () => {
+    it('should return the initial state', () => {
+      expect(videos(undefined, {})).toEqual(Immutable.List());
+    })
+    it('should handle UPDATE_VIDEOS action', () => {
+      const state = Immutable.List();
+      const mockVideos = range(1, 15).map(i => {
+        return {title: `Video ${i}`};
+      });
+      const exampleVideos = Immutable.List(mockVideos.map(video => Immutable.Map(video)));
+
+      expect(videos(state, {type: UPDATE_VIDEOS, videos: exampleVideos}))
+        .toEqual(exampleVideos);
     })
   })
 })
