ff53288e93091847eb6d2b238e1324e77f73166d
Duncan Regan <duncanregan@gmail.com>
2016-02-20 21:52:06 -0500

    need to map the videos to an immutable structure

--- js/actions/videos.js
+++ js/actions/videos.js
@@ -5,6 +5,6 @@ export const UPDATE_VIDEOS = 'UPDATE_VIDEOS';
 
 /* Action Creators */
 export const updateVideos = (videoData) => {
-  const videos = Immutable.List(videoData);
+  const videos = Immutable.List(videoData.map(video => Immutable.Map(video)));
   return {type: UPDATE_VIDEOS, videos};
 }
