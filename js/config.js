d1d7c3d7a84dca3bd482e370f4795ed90ea5bdba
Duncan Regan <duncanregan@gmail.com>
2016-02-21 00:56:36 -0500

    exporting individual config

--- js/config.js
+++ js/config.js
@@ -1,4 +1,3 @@
-export default config = {
-  appUrl: window.location.protocol + '//' + window.location.host + '/',
-  authorization: 'bearer 60787a7b4e9f2a6007020cf1cb56d580'
-}
+// export const appUrl = window.location.protocol + '//' + window.location.host + '/';
+export const authToken = 'bearer 60787a7b4e9f2a6007020cf1cb56d580';
+export const vimeoApi = 'https://api.vimeo.com';
