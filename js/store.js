0888f775afeb0ce36969436cef886636c5c9674f
Duncan Regan <duncanregan@gmail.com>
2016-02-21 17:11:12 -0500

    Prefer export default at the bottom

--- js/store.js
+++ js/store.js
@@ -9,10 +9,12 @@ const app = combineReducers({
   channel, videos, currentPage, currentVideo
 });
 
-export default function configureStore(initialState) {
+const configureStore = (initialState) => {
   const store = createStore(app, initialState, compose(
     applyMiddleware(thunk),
     window.devToolsExtension ? window.devToolsExtension() : undefined
   ));
   return store;
-}
+};
+
+export default configureStore;
