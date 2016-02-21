b1fad02c50fb6c409ec3422779ef31fc9ef0b913
Duncan Regan <duncanregan@gmail.com>
2016-02-21 18:47:21 -0500

    adds the top 25 featured channels

--- js/config.js
+++ js/config.js
@@ -1,3 +1,10 @@
 // export const appUrl = window.location.protocol + '//' + window.location.host + '/';
 export const authToken = 'bearer 60787a7b4e9f2a6007020cf1cb56d580';
 export const vimeoApi = 'https://api.vimeo.com';
+export const channels = [
+  '927', '5449', '2981', '6376', '16379', 
+  '32192', '1341', '8076', '31259', '88507', 
+  '22173', '4494', '119969', '116990', '166865', 
+  '8', '5943', '41219', '58205', '491924', 
+  '321143', '283818', '127296', '25080', '238295'
+];
