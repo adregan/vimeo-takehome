813b384c16278c3c21e380a525c684c87bae7d9f
Duncan Regan <duncanregan@gmail.com>
2016-02-21 18:48:54 -0500

    fetching some initial state

--- js/index.js
+++ js/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import vimeo, { CHANNEL, VIDEOS } from './utils/vimeo';
 import configureStore from './store';
+import randomChannel from './utils/randomChannel';
+import createState from './utils/createState';
 
-const store = configureStore({});
+let initialState = {};
+const channelId = randomChannel();
+
+Promise.all([
+  vimeo(channelId, CHANNEL),
+  vimeo(channelId, VIDEOS)
+])
+.then(([channel, videos]) => {
+  initialState = createState(channel, videos);
+})
+.catch(err => console.error(err));
 
 window.onload = () => {
   ReactDOM.render(
-    <Provider store={store}>
+    <Provider store={configureStore(initialState)}>
       <div>Yo Yo Yo</div>
     </Provider>,
     document.getElementById('load'));
