e3f5a8fe15ee0c442403503d35ce073872358ffe
Duncan Regan <duncanregan@gmail.com>
2016-02-20 21:12:44 -0500

    adds a utility function, range

---
+++ js/utils/range.js
@@ -0,0 +1,20 @@
+const range = (start, end = null) => {
+  /* range: Int Int -> Array
+     will produce an array from start to end. If only given a single value,
+     length, start is 0 and end is the length.
+  */
+  if (!end) {
+    end = start;
+    start = 0;
+  }
+  if (start >= end) {
+    return [];
+  }
+  return (new Array(end + 1))
+          .join(' ')
+          .split('')
+          .map((_, i) => i)
+          .slice(start, end);
+} 
+
+export default range;
