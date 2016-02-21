f2c158f1002e872149a2962e35a5411dff635336
Duncan Regan <duncanregan@gmail.com>
2016-02-21 16:01:21 -0500

    extending vimeo utility to fetch by page number

--- js/utils/vimeo.js
+++ js/utils/vimeo.js
@@ -3,14 +3,14 @@ import { authToken, vimeoApi } from '../config';
 export const VIDEOS = 'VIDEOS';
 export const CHANNEL = 'CHANNEL';
 
-const request = (channelId, type) => {
+const request = (channelId, type, page = 1) => {
   let url;
 
   if (type === CHANNEL) {
     url = `${vimeoApi}/channels/${channelId}`;
   }
   else if (type === VIDEOS) {
-    url = `${vimeoApi}/channels/${channelId}/videos`;
+    url = `${vimeoApi}/channels/${channelId}/videos?per_page=10&page=${page}`;
   }
   else {
     throw Error(`Unsupported type: ${type}`);
