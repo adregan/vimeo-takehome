817d22bbc1d1316a26b5e1258f639b9a0a56b9a2
Duncan Regan <duncanregan@gmail.com>
2016-02-21 19:02:00 -0500

    dispatching an action after creating the store

--- js/index.js
+++ js/index.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import vimeo, { CHANNEL, VIDEOS } from './utils/vimeo';
 import configureStore from './store';
 import randomChannel from './utils/randomChannel';
-import createState from './utils/createState';
-
-let initialState = {};
-const channelId = randomChannel();
-
-Promise.all([
-  vimeo(channelId, CHANNEL),
-  vimeo(channelId, VIDEOS)
-])
-.then(([channel, videos]) => {
-  initialState = createState(channel, videos);
-})
-.catch(err => console.error(err));
+import { changeChannel } from './actions/channel';
+ 
+const store = configureStore({});
+store.dispatch(changeChannel(randomChannel()));
 
 window.onload = () => {
   ReactDOM.render(
-    <Provider store={configureStore(initialState)}>
+    <Provider store={store}>
       <div>Yo Yo Yo</div>
     </Provider>,
     document.getElementById('load'));
