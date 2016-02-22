4e2944e72105e58891c56156d4fb9a955a4f50d7
Duncan Regan <duncanregan@gmail.com>
2016-02-21 23:06:26 -0500

    builds the videos component

--- js/components/videos.js
+++ js/components/videos.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { updateCurrentVideo } from '../actions/currentVideo';
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, currentVideo, dispatch }) => {
+  videos = videos.toJS();
+  currentVideo = currentVideo.toJS();
   return (
-    <section className="videos">
-
-    </section>
+    <ul className="videos">
+      {videos.map((video, i) => {
+        return (
+          <li className="video" key={i} onClick={(e) => dispatch(updateCurrentVideo(i))}>
+            <img className="video__thumbnail" src={video.image} />
+            <p className="video__name">
+              {video.name}
+            </p>
+            <p className="video__meta">
+              from <strong>{video.creator}</strong>
+            </p>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
 const select = (state) => {
   return {
-    videos: state.videos
+    videos: state.videos,
+    currentVideo: state.currentVideo
   };
 };
 
