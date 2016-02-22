f9aeea950921cdadb99fe68ff26bf31b260dbf10
Duncan Regan <duncanregan@gmail.com>
2016-02-21 20:24:19 -0500

    adds a test for changeChannel and uses new mock data

--- test/asyncActions.js
+++ test/asyncActions.js
@@ -1,24 +1,42 @@
 /*eslint no-undef: 0*/
-
-import Immutable from 'immutable';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
+import { UPDATE_VIDEOS } from '../js/actions/videos';
 import { UPDATE_CURRENT_VIDEO, updateCurrentVideo } from '../js/actions/currentVideo';
-import range from '../js/utils/range';
+import { UPDATE_CURRENT_PAGE } from '../js/actions/pagination';
+import { changeChannel, UPDATE_CHANNEL} from '../js/actions/channel';
+import fetchMock from 'fetch-mock';
+import mock from './mock';
 
 const mockStore = configureMockStore([thunk]);
-const mockVideos = Immutable.List(range(1, 15).map(i => {
-  return Immutable.Map({title: `Video ${i}`});
-}));
 
 describe('Async Action Creators', () => {
+  describe('changeChannel', (done) => {
+    it('should create and dispatch multiple actions to update the channel', () => {
+      const currentVideo = mock.videos.get(0);
+      const expectedActions = [
+        {type: UPDATE_CHANNEL, channel: mock.channel},
+        {type: UPDATE_VIDEOS, videos: mock.videos},
+        {type: UPDATE_CURRENT_PAGE, page: 1},
+        {type: UPDATE_CURRENT_VIDEO, video: mock.videos.get(0)}
+      ];
+      const store = mockStore({}, expectedActions, done);
+
+      fetchMock
+        .mock('https://api.vimeo.com/channels/12345', mock.ApiChannel)
+        .mock('https://api.vimeo.com/channels/12345/videos?per_page=10&page=1', mock.ApiVideos);
+
+      store.dispatch(changeChannel(12345));
+      fetchMock.restore();
+    });
+  });
   describe('currentVideo', () => {
     describe('updateCurrentVideo', (done) => {
       it('should create an action to update the current video', () => {
         const index = 9;
-        const video = mockVideos.get(index);
+        const video = mock.videos.get(index);
         const expectedActions = [{type: UPDATE_CURRENT_VIDEO, video}];
-        const store = mockStore({videos: mockVideos}, expectedActions, done);
+        const store = mockStore({videos: mock.videos}, expectedActions, done);
 
         store.dispatch(updateCurrentVideo(index));
       });
