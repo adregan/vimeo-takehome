b21b10cb984093325bc20389058e23430fa75c68
Duncan Regan <duncanregan@gmail.com>
2016-02-22 16:33:45 -0500

    adds animation to the loading component

--- js/components/loading.js
+++ js/components/loading.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
-const Loading = () => {
-  return (
-    <div className="loading">LOADING</div>
-  );
-};
+export class Loading extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      dots: '',
+      intervalId: undefined
+    };
+    this.animate = this.animate.bind(this);
+  }
+  animate() {
+    let dots = this.state.dots;
+    if (dots.length === 3) {
+      this.setState({dots: ''});
+    }
+    else {
+      this.setState({dots: dots + '.'});
+    }
+  }
+  componentDidMount() {
+    let intervalId = window.setInterval(this.animate, 500);
+    this.setState({intervalId});
+  }
+  componentWillUnmount() {
+    window.clearInterval(this.state.intervalId);
+  }
+  render() {
+    return (
+      <p className="loading">{this.state.dots}LOADING{this.state.dots}</p>
+    );
+  }
+}
 
 export default Loading;
