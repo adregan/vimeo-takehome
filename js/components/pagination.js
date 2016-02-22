cf04b13719d56b8e5b11be1873b91aac2aad7355
Duncan Regan <duncanregan@gmail.com>
2016-02-22 12:27:49 -0500

    building out the pagination (selected pages)

--- js/components/pagination.js
+++ js/components/pagination.js
@@ -4,23 +4,46 @@ import { pageSize } from '../config';
 import { changePage } from '../actions/pagination';
 import range from '../utils/range';
 
+const PageNumber = ({number, current, dispatch}) => {
+  let classes = (number === current) ? 
+    'page-list__button page-list__button--active' :
+    'page-list__button';
+  return (
+    <li className={classes} onClick={() => dispatch(changePage(number))}>
+      {number}
+    </li>
+  );
+};
+
 const Pagination = ({ page, channel, dispatch}) => {
   const { videoCount } = channel.toJS();
   const max = Math.ceil(videoCount / pageSize);
 
   return (
     <nav className="pagination">
-      {(page > 1) && <button onClick={() => dispatch(changePage(page - 1))}>Prev</button>}
-      <ul>
+      <ul className="page-list">
+        {(page > 1) && <li className="page-list__button" onClick={() => dispatch(changePage(page - 1))}>Prev</li>}
+        {(page >= 4) && <li className="page-list__button" onClick={() => dispatch(changePage(1))}>1</li>}
+        {(page >= 5) && <li className="page-list__ellipses">…</li>}
         {range(1, max + 1).map(pageNumber => {
-          return (
-            <li key={pageNumber} onClick={() => dispatch(changePage(pageNumber))}>
-              {pageNumber}
-            </li>
-          );
+          let display = false;
+          if (page === 1 && pageNumber <= 6) {
+            display = true;
+          }
+          else if (page > 1 && pageNumber >= page - 2 && pageNumber <= page + 2) {
+            display = true;
+          }
+          else if (page === max && pageNumber >= (max - 6)) {
+            display = true;
+          }
+          if (display) {
+            return <PageNumber key={pageNumber} current={page} number={pageNumber} dispatch={dispatch} />;
+          }
         })}
+      {(page <= max - 5) && <li className="page-list__ellipses">…</li>}
+      {(page <= max - 4) && <li key="last-page" className="page-list__button" onClick={() => dispatch(changePage(max))}>{max}</li>}
+      {(page < max) && <li key="next-page" className="page-list__button" onClick={() => dispatch(changePage(page + 1))}>Next</li>}
       </ul>
-      {(page < max) && <button onClick={() => dispatch(changePage(page + 1))}>Next</button>}
     </nav>
   );
 };
