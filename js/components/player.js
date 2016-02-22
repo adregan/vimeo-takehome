dbef07466f6e39ca2e8ce7b8a11cadd327c2cf7f
Duncan Regan <duncanregan@gmail.com>
2016-02-22 14:18:37 -0500

    reorganizing the player

--- js/components/player.js
+++ js/components/player.js
@@ -1,35 +1,43 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import marked from 'marked';
 
 const Player = ({video}) => {
   video = video.toJS();
   return (
     <section className="player">
-      <h1 className="video__title">
-        {video.name}
-      </h1>
-      <h2 className="video__user">
-        from <a href={video.creatorLink}>{video.creator}</a>
-      </h2>
-      <article className="video__embed" dangerouslySetInnerHTML={{__html: video.embed}} />
-      <p className="video__description">{video.description}</p>
-      <footer className="video__footer">
-        <p>Uploaded {video.createdAgo}</p>
-        <ul className="video__stats">
-          <li className="video__stat video__stat--views">
+      <article className="player__embed" dangerouslySetInnerHTML={{__html: video.embed}} />
+      
+      <aside className="player__details">
+        <div className="video-meta">
+          <h1 className="video-meta__title">
+            {video.name}
+          </h1>
+          <h2 className="video-meta__user">
+            from <a href={video.creatorLink}>{video.creator}</a>
+          </h2>
+          <p className="video-meta__description" dangerouslySetInnerHTML={{__html: video.description && marked(video.description, {sanitize: true})}}/>
+        </div>
+        <ul className="video-stats">
+          <li className="video-stats__stat video-stats__stat--uploaded">
+            Uploaded {video.createdAgo}
+          </li>
+          <li className="video-stats__stat video-stats__stat--views">
             {video.plays} Plays
           </li>
-          <li className="video__stat video__stat--likes">
+          <li className="video-stats__stat video-stats__stat--likes">
             {video.likes} Likes
           </li>
-          <li className="video__stat video__stat--comments">
+          <li className="video-stats__stat video-stats__stat--comments">
             {video.comments} Comments
           </li>
+          <li className="video-stats__stat video-stats__stat--link">
+            # <a href={video.link}>{video.link && video.link.replace('https://', '')}</a>
+          </li>
         </ul>
-        <p className="video__link">
-          # <a href={video.link}>{video.link && video.link.replace('https://', '')}</a>
-        </p>
-      </footer>
+
+      </aside>
+
     </section>
   );
 };
