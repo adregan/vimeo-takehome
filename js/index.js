5b000c04f3fd7ac1d4a1fe24343605a0f2c27ee4
Duncan Regan <duncanregan@gmail.com>
2016-02-21 22:50:44 -0500

    integrating the App component

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import configureStore from './store';
 import randomChannel from './utils/randomChannel';
 import { changeChannel } from './actions/channel';
+import App from './components/app';
  
 const store = configureStore({});
 store.dispatch(changeChannel(randomChannel()));
@@ -11,7 +12,7 @@ store.dispatch(changeChannel(randomChannel()));
 window.onload = () => {
   ReactDOM.render(
     <Provider store={store}>
-      <div>Yo Yo Yo</div>
+      <App />
     </Provider>,
     document.getElementById('load'));
 };
