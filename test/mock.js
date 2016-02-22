3ef0a2234362994c77416b6d3bb3d30e7c12eb93
Duncan Regan <duncanregan@gmail.com>
2016-02-21 20:22:14 -0500

    adds some mock data

---
+++ test/mock.js
@@ -0,0 +1,66 @@
+import Immutable from 'immutable';
+import range from '../js/utils/range';
+import { parseVideos, parseChannel } from '../js/utils/parse';
+
+const ApiVideos = range(1, 11).map(i => {
+  return { 
+    name: `Video ${i}`, 
+    description: 'Hello Hello',
+    created_time: '2016-02-21T23:09:04+00:00',
+    embed: {
+      html: '<embed />'
+    },
+    link: 'https://example.com',
+    pictures: {
+      sizes: [{link: ''}, {link: ''}, {link: ''}]
+    },
+    user: {
+      name: 'Duncan Regan'
+    },
+    stats: {
+      plays: 123
+    },
+    metadata: {
+      connections: {
+        likes: {total: 10},
+        comments: {total: 20}
+      }
+    }
+  };
+});
+
+const videoData = parseVideos(ApiVideos);
+const videos = Immutable.List(videoData.map(video => {
+  return Immutable.Map(video);
+}));
+
+const ApiChannel = {
+  name: 'Cool Channel',
+  header: {
+    sizes: [{link: 'http://cool.png'}]
+  },
+  created_time: '2016-02-21T23:09:04+00:00',
+  metadata: {
+    connections: {
+      users: {total: 200},
+      videos: {total: 100}
+    }
+  },
+  user: {
+    name: 'Duncan',
+    link: 'https://greatlink.com',
+    pictures: {sizes: [{link: 'https://anothergreatlink.com'}]}
+  }
+};
+
+const channelData = parseChannel(ApiChannel);
+const channel = Immutable.Map(channelData);
+
+export default {
+  ApiVideos,
+  videoData,
+  videos,
+  ApiChannel,
+  channelData,
+  channel
+};
