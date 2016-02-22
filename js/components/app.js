479c4cf742af352ed477b13cbb151cce0c66f653
Duncan Regan <duncanregan@gmail.com>
2016-02-21 23:44:54 -0500

    bringing in the pagination and channel info components

--- js/components/app.js
+++ js/components/app.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import CurrentVideo from './currentVideo';
-// import ChannelInfo from './channelInfo';
-// import Pagination from './pagination';
+import ChannelInfo from './channelInfo';
+import Pagination from './pagination';
 import Videos from './videos';
 
 const App = () => {
   return (
     <div className="app">
+      <ChannelInfo />
       <CurrentVideo />
       <Videos />
+      <Pagination />
     </div>
   );
 };
